132f478a6e841be5714a7e8680eb621fd5a2cf75
Rémi Duriez <remi@duriez.re>
2022-05-03 22:43:19 +0200

    Reindent file

--- www/index.php
+++ www/index.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
-  <head>
-     <title>Duriez R&eacute;mi</title>
-	 <link type="text/css" rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <header>
-      <h1><span class="machine">duriez.re</span><span class="human">mi</span></h1>
-	  <p><span>&#128188; D&eacute;veloppeur</span><span>&#128204 Lille</span></p>
-    </header>
-	<section>
-	  <div><a href="https://www.facebook.com/duriez.remi" rel="me">&#128101;</a></div>
-	  <div><a href="https://www.linkedin.com/in/duriez" rel="me">&#128084;</a></div>
-	  <div><a href="https://www.last.fm/fr/user/RemiDu" rel="me">&#127911;</a></div>
-	  <div><a href="https://www.betaseries.com/membre/remi" rel="me">&#128250;</a></div>
-	  <div><a href="https://www.gamekult.com/membre/remidu-1241161/collection" rel="me">&#127918;</a></div>
-	  <div><a href="https://leagueofcomicgeeks.com/profile/remi" rel="me">&#128218;</a></div>
-	  <div><a href="https://myffbad.fr/joueur/07012169" rel="me">&#127992;</a></div>
-	  <div><a href="https://untappd.com/user/remidu" rel="me">&#127867;</a></div>
-	</section>
-  </body>
+    <head>
+        <title>Duriez R&eacute;mi</title>
+        <link type="text/css" rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1><span class="machine">duriez.re</span><span class="human">mi</span></h1>
+            <p><span>&#128188; D&eacute;veloppeur</span><span>&#128204 Lille</span></p>
+        </header>
+        <section>   
+            <div><a href="https://www.facebook.com/duriez.remi" rel="me">&#128101;</a></div>
+            <div><a href="https://www.linkedin.com/in/duriez" rel="me">&#128084;</a></div>
+            <div><a href="https://www.last.fm/fr/user/RemiDu" rel="me">&#127911;</a></div>
+            <div><a href="https://www.betaseries.com/membre/remi" rel="me">&#128250;</a></div>
+            <div><a href="https://www.gamekult.com/membre/remidu-1241161/collection" rel="me">&#127918;</a></div>
+            <div><a href="https://leagueofcomicgeeks.com/profile/remi" rel="me">&#128218;</a></div>
+            <div><a href="https://myffbad.fr/joueur/07012169" rel="me">&#127992;</a></div>
+            <div><a href="https://untappd.com/user/remidu" rel="external">&#127867;</a></div>
+        </section>
+    </body>
 </html>
